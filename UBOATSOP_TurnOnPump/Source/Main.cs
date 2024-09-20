daf32a61bf67db1a5252dfb79dab4e2a90ca1ff9
Dejan Macesic <dejan@base64.com>
2024-09-20 11:37:39 -0400

    Pump check once an hour and water level check

--- UBOATSOP_TurnOnPump/Source/Main.cs
+++ UBOATSOP_TurnOnPump/Source/Main.cs
@@ -1,4 +1,5 @@
-﻿using DWS.Common.InjectionFramework;
+﻿using HarmonyLib;
+using DWS.Common.InjectionFramework;
 using UBOAT.Game;
 using UBOAT.Game.Core;
 using UBOAT.Game.UI;
@@ -8,10 +9,12 @@ using UnityEngine;
 using System;
 using UBOAT.Game.Scene.Entities;
 using UBOAT.Game.Core.Data;
-using UBOAT.Game.Scene.Characters.Actions;
 using UBOAT.Game.Scene.Items;
+using UBOAT.Game.Core.Serialization;
+using UBOAT.Game.Scene.Characters.Actions;
 
-public class UBOATSOP_TurnOnPump : BackgroundTaskBase
+
+public class UBOATSOP_TurnOnPump : BackgroundTaskBase, IUserMod
 {
     [Inject] private static IExecutionQueue executionQueue;
     [Inject] private static INotificationBarUI notificationBarUI;
@@ -21,15 +24,28 @@ public class UBOATSOP_TurnOnPump : BackgroundTaskBase
     [Inject] private static UserSettings userSettings;
 
     public const string Version = UBOATSOP_TurnOnPump_Constants.Version;
+    public const float INTERVAL_UPDATE_SECONDS = 60.0f;  // run once per minute
+    public const float INTERVAL_CHECK_SECONDS = 3600.0f; // check pump every hour
+    public const double MAXIMUM_WATER_LEVEL = 0.5;
+
+    private static float secondsElapsedSinceLastCheck = 0f;
     private static bool firstUpdate = true;
-    
+
+    [NonSerializedInGameState]
+    public void OnLoaded()
+    {
+        var harmony = new Harmony("com.andargor.UBOATSOP_TurnOnPump");
+        harmony.PatchAll();
+        Debug.Log($"{this} Version {Version} Harmony Loaded");
+    }
+
     public override void Start()
     {
         try
         {
             Debug.Log($"{this} Version {Version}");
             firstUpdate = true;
-            executionQueue.AddTimedUpdateListener(DoUpdate, 5.0f);
+            executionQueue.AddTimedUpdateListener(DoUpdate, INTERVAL_UPDATE_SECONDS);
         } catch (Exception ex)
         {
             Debug.LogException(ex);
@@ -64,29 +80,69 @@ public class UBOATSOP_TurnOnPump : BackgroundTaskBase
 
     }
 
+    private static double TrimPumpWaterAmount(TrimPump pump)
+    {
+        double result = 0.0;
+
+        try
+        {
+            if (pump)
+            {
+                TrimPump.ConnectedTank[] tanks = (TrimPump.ConnectedTank[])(TrimPump.ConnectedTank[])Traverse.Create(pump)?.Field("tanks")?.GetValue();
+
+                if (tanks != null)
+                {
+                    for (int i = 0; i < tanks.Length; i++)
+                    {
+                        result += tanks[i].Tank.Water;
+                    }
+                }
+                //Debug.Log($"UBOATSOP_TurnOnPump TrimPumpWaterAmount PUMP {pump} TANKS {tanks} AMOUNT {result}");
+            }
+        } catch (Exception ex)
+        {
+            Debug.LogException(ex);
+        }
+
+        return result;
+    }
 
     private static void TurnOnPump()
     {
+
+        secondsElapsedSinceLastCheck += INTERVAL_UPDATE_SECONDS;
+        if (secondsElapsedSinceLastCheck > float.MaxValue) secondsElapsedSinceLastCheck = 0;
+
         if (playerShipProxy != null && playerShipProxy.CurrentShip != null
                 )
         {
             TrimPump equipment = playerShipProxy.CurrentShip.GetEquipment<TrimPump>("Trim Pump");
             if ((bool)(UnityEngine.Object)equipment)
             {
-                if (equipment.IsWaterLeft)
+                double waterLevel = TrimPumpWaterAmount(equipment);
+                //Debug.Log($"UBOATSOP_TurnOnPump WATER LEVEL {waterLevel}");
+                
+                if (waterLevel >= MAXIMUM_WATER_LEVEL)
                 {
                     if (!equipment.enabled 
                         && !playerShipProxy.CurrentShip.Alarmed
                         && !playerShipProxy.CurrentShip.SubmergedOrGoingToSubmerge)
                     {
-                        UBOAT.Game.Scene.Characters.Activators.Activator component = equipment.GetComponent<UBOAT.Game.Scene.Characters.Activators.Activator>();
-                        UBOAT.Game.Scene.Characters.Skills.CharacterActionInfo action = component.GetAction<SwitchPowerAction>((PlayableCharacter)null, true);
-                        if (action.Action != null && action.State == ActionState.Enabled)
-                            playerCrew.GlobalActionQueue.Add(action.Action.Duplicate(component));
+                        if (secondsElapsedSinceLastCheck > INTERVAL_CHECK_SECONDS)
+                        {
+                            UBOAT.Game.Scene.Characters.Activators.Activator component = equipment.GetComponent<UBOAT.Game.Scene.Characters.Activators.Activator>();
+                            UBOAT.Game.Scene.Characters.Skills.CharacterActionInfo action = component.GetAction<SwitchPowerAction>((PlayableCharacter)null, true);
+                            if (action.Action != null && action.State == ActionState.Enabled)
+                            {
+                                Debug.Log($"UBOATSOP_TurnOnPump ENABLE WATER LEVEL {waterLevel}");
+                                playerCrew.GlobalActionQueue.Add(action.Action.Duplicate(component));
+                                secondsElapsedSinceLastCheck = 0f;
+                            }
+                        }
                     }
                 } else
                 {
-                    if (equipment.enabled)
+                    if (!equipment.IsWaterLeft && equipment.enabled)
                     {
                         UBOAT.Game.Scene.Characters.Activators.Activator component = equipment.GetComponent<UBOAT.Game.Scene.Characters.Activators.Activator>();
                         UBOAT.Game.Scene.Characters.Skills.CharacterActionInfo action = component.GetAction<SwitchPowerAction>((PlayableCharacter)null, true);
@@ -94,6 +150,7 @@ public class UBOATSOP_TurnOnPump : BackgroundTaskBase
                             playerCrew.GlobalActionQueue.Add(action.Action.Duplicate(component));
                     }
                 }
+                
             }
         }
     }
