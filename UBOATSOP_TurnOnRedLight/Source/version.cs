75632c3220318bb251c59c22d71e350423b09939
Andargor <andargor@yahoo.com>
2026-06-16 15:21:53 -0400

    TORL switch conditions reinforced

--- UBOATSOP_TurnOnRedLight/Source/version.cs
+++ UBOATSOP_TurnOnRedLight/Source/version.cs
@@ -1,1 +1,1 @@
-static class UBOATSOP_TurnOnRedLight_Constants { public const string Version = "2024.1.13.15"; }
+static class UBOATSOP_TurnOnRedLight_Constants { public const string Version = "2024.1.13.16"; }
