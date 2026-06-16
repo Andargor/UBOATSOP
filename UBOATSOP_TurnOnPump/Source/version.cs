f387ecef1f2b83644be35389ce25c572458439e1
Andargor <andargor@yahoo.com>
2026-06-16 15:21:54 -0400

    Pump check once an hour and water level check

--- UBOATSOP_TurnOnPump/Source/version.cs
+++ UBOATSOP_TurnOnPump/Source/version.cs
@@ -1,1 +1,1 @@
-static class UBOATSOP_TurnOnPump_Constants { public const string Version = "2024.1.13.12"; }
+static class UBOATSOP_TurnOnPump_Constants { public const string Version = "2024.1.14.27"; }
