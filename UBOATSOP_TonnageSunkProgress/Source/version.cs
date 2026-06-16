3ab4e323bcb3c20e59e90858bd853447b0fa1e1f
Andargor <andargor@yahoo.com>
2026-06-16 15:21:57 -0400

    full localization of tonnage string

--- UBOATSOP_TonnageSunkProgress/Source/version.cs
+++ UBOATSOP_TonnageSunkProgress/Source/version.cs
@@ -1,1 +1,1 @@
-static class UBOATSOP_TonnageSunkProgress_Constants { public const string Version = "2024.1.14.7"; }
+static class UBOATSOP_TonnageSunkProgress_Constants { public const string Version = "2024.1.22.12"; }
